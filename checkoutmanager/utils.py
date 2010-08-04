8b1979eca9fa814b834e939c67c76f2b90e337aa
Reinout van Rees <reinout@vanrees.org>
2010-08-04 21:16:46 +0200

    Checking return code from executed commands.  On error, the command and working directory is printed and also the output.  And the script stops right away.  Fixes #5.

--- checkoutmanager/utils.py
+++ checkoutmanager/utils.py
@@ -8,7 +8,7 @@ MUST_CLOSE_FDS = not sys.platform.startswith('win')
 VERBOSE = False
 
 
-def system(command, input=''):
+def system(command, input=None):
     """commands.getoutput() replacement that also works on windows
 
     Code copied from zc.buildout.
@@ -22,11 +22,16 @@ def system(command, input=''):
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE,
                          close_fds=MUST_CLOSE_FDS)
-    i, o, e = (p.stdin, p.stdout, p.stderr)
-    if input:
-        i.write(input)
-    i.close()
-    result = o.read() + e.read()
-    o.close()
-    e.close()
+    stdoutdata, stderrdata = p.communicate(input=input)
+    result = stdoutdata + stderrdata
+    if p.returncode:
+        # An error occured!  Notify and bail out directly.
+        print "Something went wrong when executing:"
+        print "    %s" % command
+        print "while in directory:"
+        print "    %s" % os.getcwd
+        print "Output:"
+        print result
+        sys.exit(1)
+
     return result
