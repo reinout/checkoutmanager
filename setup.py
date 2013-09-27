53b0e6134a1fd4913bf48904d945c0d56b5c0a61
Reinout van Rees <reinout@vanrees.org>
2013-09-27 14:15:27 +0200

    Preparing release 1.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-version = '1.15.dev0'
+version = '1.15'
 
 sample_config = open(os.path.join('checkoutmanager',
                                   'sample.cfg')).readlines()
