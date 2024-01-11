251dccde9049a33bdab9eb821f83e62dc814ec1d
Reinout van Rees <reinout@vanrees.org>
2024-01-11 10:43:44 +0100

    Cleaned up commented-out old code

--- checkoutmanager/tests/conftest.py
+++ checkoutmanager/tests/conftest.py
@@ -1,12 +1,7 @@
 import os
 from pathlib import Path
 
-# import re
-# import shutil
 import pytest
-
-# import z3c.testsetup
-# from zope.testing import renormalizing
 
 
 @pytest.fixture()
@@ -17,33 +12,3 @@ def homedir_in_tmp(tmp_path: Path):
     os.environ["HOME"] = str(homedir)
     yield homedir
     os.environ["HOME"] = orig_home
-
-
-# checker = renormalizing.RENormalizing(
-#     [
-#         # Mock homedir in temp (with weird /private prefix for OSX).
-#         (re.compile("%s[^/]+" % re.escape("/private" + _tempdir)), "HOMEDIR"),
-#         # Mock homedir in temp (regular one).
-#         (re.compile("%s[^/]+" % re.escape(_tempdir)), "HOMEDIR"),
-#     ]
-# )
-
-
-# def setup(test):
-#     test.homedir = tempfile.mkdtemp(prefix="homedir")
-#     test.orig_home = os.environ.get("HOME")
-#     os.environ["HOME"] = test.homedir
-#     test.globs["homedir"] = test.homedir
-
-
-# def teardown(test):
-#     shutil.rmtree(test.homedir)
-#     os.environ["HOME"] = test.orig_home
-
-
-# test_suite = z3c.testsetup.register_all_tests(
-#     "checkoutmanager",
-#     setup=setup,
-#     teardown=teardown,
-#     checker=checker,
-# )
